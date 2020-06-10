cdc79cebdc1e421a3a498542b441463711fc3a7e
aaronmorrison1 <59827533+aaronmorrison1@users.noreply.github.com>
2020-06-10 12:54:04 +0100

    Added develop branch and develop build branch.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,8 +1,11 @@
 package com.napier.sem;
 
-public class App {
-    public static void main(String[] args)
-    {
-        System.out.println("Boo yah!");
+public class App
+{
+    public static void main(String[] args) {
+
+        System.out.println("Heyyyyyy");
+
     }
+
 }
